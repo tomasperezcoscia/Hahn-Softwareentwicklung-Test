94c45edbd73ee5c03763caaa162e0fd826c32f0f
tomasperezcoscia <perezcosciatomas@gmail.com>
2023-02-03 13:11:19 -0300

    FileUploader error handling
    Some simple error handling and logging

--- Hahn Softwareentwicklung/Controllers/FileUploaderController.cs
+++ Hahn Softwareentwicklung/Controllers/FileUploaderController.cs
@@ -8,10 +8,12 @@ namespace Hahn_Softwareentwicklung;
 public class FileUploaderController : ControllerBase
 {
     private readonly IWebHostEnvironment _hostingEnvironment;
+    private readonly ILogger<FileUploaderController> _logger;
 
-    public FileUploaderController(IWebHostEnvironment hostingEnvironment)
+    public FileUploaderController(IWebHostEnvironment hostingEnvironment, ILogger<FileUploaderController> logger)
     {
         _hostingEnvironment = hostingEnvironment;
+        _logger = logger;
     }
 
     [HttpPost]
@@ -19,15 +21,25 @@ public class FileUploaderController : ControllerBase
     {
         if (file == null || file.Length == 0)
         {
+            _logger.LogError("File is empty or not provided");
             return BadRequest("File is empty or not provided");
         }
 
         string fileName = Path.GetFileName(file.FileName);
         string filePath = Path.Combine(_hostingEnvironment.WebRootPath, "files", fileName);
 
-        using (var stream = new FileStream(filePath, FileMode.Create))
+        try
         {
-            await file.CopyToAsync(stream);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            _logger.LogInformation($"File {fileName} uploaded successfully.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error uploading file {fileName}.");
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error uploading file.");
         }
 
         return Ok();
